17ae72ffb3bb6a7a89f3570d9a03687eaf6a3ed9
commonbrushtail <commonbrushtail@users.noreply.github.com>
2020-08-13 19:44:41 +0700

    13

--- index.php
+++ index.php
@@ -1,8 +1,23 @@
 <?php 
 
+define('NAME','Tae');
+
 echo 'hello, ninjas';
+$age = '[lqaw;ldkas;ldask';
+$number = 5464;
+$stringOne = 'my email is ';
+$stringTwo = 'asdpkfpoa@gmail.com';
 
+echo strlen(NAME);
 
+$array = [1,2,3,4,5,6,7];
+$arrayString = ['ttat','asdasd','asdasd'];
+print_r($array);
+print_r($arrayString[2]);
+$arrayString[2] = "test";
+print_r($arrayString);
+$array[]= 50;
+print_r($array);
 ?>
 
 
@@ -15,7 +30,10 @@ echo 'hello, ninjas';
 </head>
 <body>
 
-    <h1><?php echo 'hello,ninjas';?></h1>
-    
+    <h1><?php echo $age, $number?></h1>
+    <h2><?php echo NAME;?></h2>
+    <h2><?php echo $stringOne . $stringTwo;?></h2>
+    <h2><?php echo $stringTwo;?></h2>
+    <h3><?php echo "the ninja scream $NAME";?> </h3>    
 </body>
 </html>
