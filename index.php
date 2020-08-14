38d38de6e0fca6c2dc7a7375d12611aefd159263
commonbrushtail <commonbrushtail@users.noreply.github.com>
2020-08-14 09:56:29 +0700

    end for each in html example

--- index.php
+++ index.php
@@ -1,25 +1,26 @@
+
+
+
 <?php 
 
-define('NAME','Tae');
+$all = ['title'=>'mario party', 'publisher'=> 'nintendo'];
 
-echo 'hello, ninjas';
-$age = '[lqaw;ldkas;ldask';
-$number = 5464;
-$stringOne = 'my email is ';
-$stringTwo = 'asdpkfpoa@gmail.com';
 
-echo strlen(NAME);
 
-$array = [1,2,3,4,5,6,7];
-$arrayString = ['ttat','asdasd','asdasd'];
-print_r($array);
-print_r($arrayString[2]);
-$arrayString[2] = "test";
-print_r($arrayString);
-$array[]= 50;
-print_r($array);
+
+$test = [1,2,3,4,5,6,7,8,9,1,0,1,2,4];
+
+
+
+
+
+
+
+
+
+
+
 ?>
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -27,13 +28,23 @@ print_r($array);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+
+    
+
 </head>
 <body>
+    
+    <ul>
+    <?php foreach ($test as $key) : ?>
+      
+      <li><?php echo $key; ?></li>
 
-    <h1><?php echo $age, $number?></h1>
-    <h2><?php echo NAME;?></h2>
-    <h2><?php echo $stringOne . $stringTwo;?></h2>
-    <h2><?php echo $stringTwo;?></h2>
-    <h3><?php echo "the ninja scream $NAME";?> </h3>    
+
+    <?php endforeach;?>
+
+
+    </ul>
+
+    
 </body>
 </html>
