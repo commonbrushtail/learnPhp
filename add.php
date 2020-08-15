1be1bea36929897de4d2c15a81c8d1571f526beb
commonbrushtail <commonbrushtail@users.noreply.github.com>
2020-08-15 15:23:55 +0700

    MYSQLI example

--- add.php
+++ add.php
@@ -2,36 +2,40 @@
     $title = $email = $ingredients = '';
     $error = ['email'=>'','title'=>'','ingredients'=>''];
     
-        if(isset($_POST['submit'])){
-            if(empty($_POST['email'])){
-                $error['email']='please enter email';
-            } else {
-                $email = $_POST['email'];
-                if (!filter_var($email,FILTER_VALIDATE_EMAIL)){
-                   $error['email'] = 'email is not valid';
-                }
+    if(isset($_POST['submit'])){
+        if(empty($_POST['email'])){
+            $error['email']='please enter email';
+        } else {
+            $email = $_POST['email'];
+            if (!filter_var($email,FILTER_VALIDATE_EMAIL)){
+                $error['email'] = 'email is not valid';
             }
-    
-            if(empty($_POST['ingredients'])){
-                $error['ingredients']='ingredients is required';
-            } else {
-                $ingredients = $_POST['ingredients'];
-                if(!preg_match('/^([a-zA-Z\s]+)(,\s*[a-zA-Z\s]*)*$/',$ingredients)){
-                    $error['ingredients'] = 'ingredients must be seperated by comma';
-                }
-            }
-    
-            if(empty($_POST['title'])){
-                $error['title'] = 'title is required';
-            } else {
-                $title = $_POST['title'];
-                if(!preg_match('/^[a-zA-Z\s]+$/',$title)){
-                    $error['title'] = 'title must be letter and space only';
-                }
-            }
-    
-            
         }
+
+        if(empty($_POST['ingredients'])){
+            $error['ingredients']='ingredients is required';
+        } else {
+            $ingredients = $_POST['ingredients'];
+            if(!preg_match('/^([a-zA-Z\s]+)(,\s*[a-zA-Z\s]*)*$/',$ingredients)){
+                $error['ingredients'] = 'ingredients must be seperated by comma';
+            }
+        }
+
+        if(empty($_POST['title'])){
+            $error['title'] = 'title is required';
+        } else {
+            $title = $_POST['title'];
+            if(!preg_match('/^[a-zA-Z\s]+$/',$title)){
+                $error['title'] = 'title must be letter and space only';
+            }
+        }
+
+        if(array_filter($error)){
+            echo 'there is an error';
+        } else echo 'the form have submit';
+
+        
+    }
 
         
 
@@ -45,30 +49,32 @@
 <!DOCTYPE html>
 <html lang="en">
 <?php include 'head.php'?>
+
 <body>
 
-<?php include 'components/navBar.php' ?>
+    <?php include 'components/navBar.php' ?>
 
-<section class="addPizzaContainer">
-    <h1>Add a Pizza</h1>
-    <form class="addPizza" action="add.php" method="POST">
-        <label for="">Your Email</label>
-        <div><?php echo $error['email']; ?></div>
-        <input type="text" name="email" value = "<?php echo $email;?>">
-        <label for="">Pizza Title</label>
-        <div><?php echo $error['title']; ?></div>
-        <input type="text" name="title" value = "<?php echo $title;?>">
-        <label for="">ingredients</label>
-        <div><?php echo $error['ingredients']; ?></div>
-        <input type="text" name="ingredients" value ="<?php echo $ingredients;?>">
-        <button type="submit" name="submit" value="submit">Submit</button>
-    </form>
+    <section class="addPizzaContainer">
+        <h1>Add a Pizza</h1>
+        <form class="addPizza" action="add.php" method="POST">
+            <label for="">Your Email</label>
+            <div><?php echo $error['email']; ?></div>
+            <input type="text" name="email" value="<?php echo $email;?>">
+            <label for="">Pizza Title</label>
+            <div><?php echo $error['title']; ?></div>
+            <input type="text" name="title" value="<?php echo $title;?>">
+            <label for="">ingredients</label>
+            <div><?php echo $error['ingredients']; ?></div>
+            <input type="text" name="ingredients" value="<?php echo $ingredients;?>">
+            <button type="submit" name="submit" value="submit">Submit</button>
+        </form>
 
-</section>
-
-
+    </section>
 
 
-<?php include 'components/footer.php'?>
+
+
+    <?php include 'components/footer.php'?>
 </body>
+
 </html>
--- index.php
+++ index.php
@@ -1,3 +1,22 @@
+<?php
+$conn = mysqli_connect('localhost','tae','1234','pizza');
+
+if(!$conn){
+    echo 'error no connection' . mysqli_connect_error();
+}
+
+$sql = 'SELECT title,ingredients,id FROM pizza';
+$result = mysqli_query($conn,$sql);
+
+$pizza = mysqli_fetch_all($result,MYSQLI_ASSOC);
+
+mysqli_free_result($result);
+mysqli_close($conn);
+
+print_r ($pizza);
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <?php include 'head.php'?>
